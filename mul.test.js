2cdbf41af2bcb7b9c18205daf58b9ca9d2426595
unknown <michaeladdis78@gmail.com>
2025-05-07 17:48:16 +0300

    add mul test

---
+++ mul.test.js
@@ -0,0 +1,9 @@
+const mul = require('./ multiply');
+
+test('multipley 2 * 2 to equl 4', () => {
+    expect(mul(2, 2,)).toBe(4);
+})
+
+test('multiply 2 * 0 to equlal 0', () => {
+    expect(mult(2,0).toBe(1));
+})
